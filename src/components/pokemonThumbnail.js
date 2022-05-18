f105975d4a5eb2b3fd253a2fa05942adf3cdad10
wooldridge12 <wooldridgemikey@gmail.com>
2022-05-18 10:36:48 -0400

    Created a card look with different color backgrounds depending on the pokemon type.

--- src/components/pokemonThumbnail.js
+++ src/components/pokemonThumbnail.js
@@ -4,15 +4,16 @@ const PokemonThumbnail = ({id, name, image, type}) => {
     const style = `thumbContainer ${type}`
     return (
         <div className={style}>
+        <div className="pokemonCard">
             <div class="number">
                 <small>#0{id}</small>
             </div>
             <img src={image} alt={name} />
             <div>
-                <h3>{name}</h3>
-                <small>Type: {type}</small>
+                <h3 className='pokemonName'>{name}</h3>
+                <small className='pokemonType'>Type: {type}</small>
             </div>
-
+            </div>
         </div>
     )
 }
